e24bf1af54caf511b2ef7c6069c4da462d2a91d2
Şuayip Işık <suayip.isik.16@gmail.com>
2023-01-09 00:19:32 +0300

    first commit

---
+++ src/main/java/kodlamaio/northwind/entities/concretes/Product.java
@@ -0,0 +1,26 @@
+package kodlamaio.northwind.entities.concretes;
+
+import javax.persistence.Entity;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Product {
+	private int id;
+	private int categoryId;
+	private String productName;
+	private double unitPrice;
+	private short unitsInStock;
+	private String quantityPerUnit;
+	
+	public Product(int id, int categoryId, String productName, double unitPrice, short unitsInStock, String quantityPerUnit) {
+		super();
+		this.id = id;
+		this.categoryId = categoryId;
+		this.productName = productName;
+		this.unitPrice = unitPrice;
+		this.unitsInStock = unitsInStock;
+		this.quantityPerUnit = quantityPerUnit;
+	}
+}
